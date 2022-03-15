a39e1807b79b534e02591ff4918c1be0d6cc3a1a
N0tGoingPro <87959691+N0tGoingPro@users.noreply.github.com>
2022-03-15 14:03:10 -0700

    Update SpellingMistakes.py
    changed SpellingMistakes because of the complexity involved in GUI

--- SpellingMistakes.py
+++ SpellingMistakes.py
@@ -1,23 +1,25 @@
-from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
-
-#stop words are words like the, they and then
-stopWords = set(stopwords.words('english'))
+from nltk.corpus import brown
 
 class SpellingMistakes:
-    def spelling_mistakes(str):
-        tokenizer = RegexpTokenizer(r'\w+')
-        sentString = tokenizer.tokenize(str)
-        count = 0
-        for words in sentString:
-            strip = words.rstrip()
-            if not wn.synsets(strip):
-                if not strip in stopWords:
-                    while(not wn.synsets(words) or words in stopWords):
-                        print("Word you spelt wrong: " + words)
-                        val = input("Spell " + words +" again correctly: ")
-                        words=val
-                        sentString[count] = val
-            count += 1
-        return sentString
+    def spelling_mistakes(userInput):
+        userInputLowerCaseSplit = userInput.split()
+        #stop words are words like the, they and then
+        stopWords = set(stopwords.words('english'))
+        # gets a list of words
+        wordList = brown.words()
+        spelledCorrect = []
+        wordSpelledIncorrect = []
+        # gose through each word and checks if it is in stopsWords or wordList, if it is, then 
+        # will set spelledCorrect to True 
+        for i in range(len(userInputLowerCaseSplit)):
+            if (userInputLowerCaseSplit[i] in stopWords) or (userInputLowerCaseSplit[i] in wordList):
+                spelledCorrect.append(True)
+            else:
+                spelledCorrect.append(False)
+                wordSpelledIncorrect.append(userInputLowerCaseSplit[i])
+        print(spelledCorrect)
+        if False in spelledCorrect:
+            return False, wordSpelledIncorrect
+        else:
+            return True, wordSpelledIncorrect
