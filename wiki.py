77940275c49a8ad02a6efcc483fed7e68001ce53
Maysey Lu <77411867+mayxxluu@users.noreply.github.com>
2022-04-06 15:01:11 -0700

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -17,19 +17,17 @@ def getWiki(sentence):
     if question:
         if len(np) != 0:
             try:
-                print(np[0])
                 summary = wikipedia.summary(np[0], sentences=1, auto_suggest=False)
-                learnMore = "\nIf you would like to learn more, please visit: " + wikipedia.page(np[0], auto_suggest=False).url
-                q = "\nIs there anything else you would like to share?"
+                learnMore = " If you would like to learn more, please visit: " + wikipedia.page(np[0], auto_suggest=False).url
+                q = ". Is there anything else you would like to share?"
                 result = summary + learnMore + q
             except wikipedia.DisambiguationError as e:
                 result = "Sorry, could you be more a little more specific? There are many Wikipedia pages with the title '" + np[0] + "' in it."
         else:
             try:
-                print(nn[0])
                 summary = wikipedia.summary(nn[0], sentences=1, auto_suggest=False)
-                learnMore = "\nIf you would like to learn more, please visit: " + wikipedia.page(nn[0], auto_suggest=False).url
-                q = "\nIs there anything else you would like to share?"
+                learnMore = " If you would like to learn more, please visit: " + wikipedia.page(nn[0], auto_suggest=False).url
+                q = ". Is there anything else you would like to share?"
                 result = summary + learnMore + q
             except wikipedia.DisambiguationError as e:
                 result = "Sorry, could you be more a little more specific? There are many Wikipedia pages with the title '" + nn[0] + "' in it."
